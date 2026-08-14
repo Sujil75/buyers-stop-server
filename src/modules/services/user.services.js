db7696d1e2585142761a413e79e31fcc08b1c3cc
Sujil S Mathew <sujilsmathew54@gmail.com>
2026-08-14 17:58:52 +0530

    added error validation for user registration and adding hashed password

--- src/modules/services/user.services.js
+++ src/modules/services/user.services.js
@@ -1,4 +1,31 @@
-const createUser = data => {
+const bcrypt = require("bcrypt");
+
+const provideInvalidData = data => {
+    const {email, username, password} = data;
+
+    if (!email) {
+        const err = new Error("Please enter an email");
+        err.status = 400;
+
+        throw err;
+    };
+
+    if (!username) {
+        const err = new Error("Please enter a username");
+        err.status = 400;
+
+        throw err;
+    };
+
+    if (!password) {
+        const err = new Error("Please enter a password");
+        err.status = 400;
+
+        throw err;
+    };
+};
+
+const createUser = async data => {
     if (!data) {
         const err = new Error("No User Data Found");
         err.status = 400;
@@ -6,7 +33,11 @@ const createUser = data => {
         throw err;
     };
 
-    console.log(data)  
+    provideInvalidData(data);
+
+    const hashedPassword = await bcrypt.hash(data.password, 10);
+
+    console.log(hashedPassword);
 };
 
 module.exports = {
