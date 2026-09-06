98c45fe11658b507940a1105f3c01df7298f404c
Sujil S Mathew <sujilsmathew54@gmail.com>
2026-09-06 13:41:30 +0530

    changing my method of traditional way of creating the backend with more efficient and clean way using oops for the 1st step starting with error handlers and a new folder named as core

---
+++ src/core/errors/index.js
@@ -0,0 +1,17 @@
+const AppError = require('./AppError');
+const BadRequestError = require('./BadRequestError');
+const NotFoundError = require('./NotFoundError');
+const UnauthorizedError = require('./UnauthorizedError');
+const ForbiddenError = require('./ForbiddenError');
+const ConflictError = require('./ConflictError');
+const ValidationError = require('./ValidationError');
+
+module.exports = {
+    AppError,
+    BadRequestError,
+    NotFoundError,
+    UnauthorizedError,
+    ForbiddenError,
+    ConflictError,
+    ValidationError,
+};
